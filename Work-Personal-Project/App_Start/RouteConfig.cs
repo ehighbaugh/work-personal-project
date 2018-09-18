f5f6c3d9bc4a20f5b674aefe2688ffe26a21f1f9
erin.highbaugh <erin.highbaugh@waystar.com>
2018-09-18 15:26:51 -0400

    Setup Azure Continuous Deployment; Added Navbar controller and partial view.

--- Work-Personal-Project/App_Start/RouteConfig.cs
+++ Work-Personal-Project/App_Start/RouteConfig.cs
@@ -12,6 +12,8 @@ namespace Work_Personal_Project
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{fileName}.jpg");
+            routes.IgnoreRoute("{fileName}.html");
 
             routes.MapRoute(
                 name: "Default",
